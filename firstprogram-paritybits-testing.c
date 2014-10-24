6142c5f9c7e70bd344da4c9fc217f7cb4e523c66
lexrobotics1 <lexrobotics@gmail.com>
2014-10-24 18:17:00 -0400

    Update firstprogram-paritybits-testing.c

--- firstprogram-paritybits-testing.c
+++ firstprogram-paritybits-testing.c
@@ -1,7 +1,7 @@
 #pragma config(Hubs,  S1, HTMotor,  HTServo,  HTMotor,  none)
 #pragma config(Sensor, S1,     ,               sensorI2CMuxController)
-#pragma config(Motor,  mtr_S1_C1_1,     motorRight,    tmotorTetrix, openLoop, encoder)
-#pragma config(Motor,  mtr_S1_C1_2,     motorLeft,     tmotorTetrix, openLoop, reversed, encoder)
+#pragma config(Motor,  mtr_S1_C1_1,     motorRight,    tmotorTetrix, openLoop, reversed, encoder)
+#pragma config(Motor,  mtr_S1_C1_2,     motorLeft,     tmotorTetrix, openLoop, encoder)
 #pragma config(Motor,  mtr_S1_C3_1,     thrower,       tmotorTetrix, openLoop)
 #pragma config(Motor,  mtr_S1_C3_2,     motorG,        tmotorTetrix, openLoop)
 #pragma config(Servo,  srvo_S1_C2_1,    grabber2,             tServoStandard)
@@ -22,6 +22,7 @@ task main()
 
     while(true){
 	   getJoystickSettings(joystick);
+
 	   if(abs(joystick.joy1_y1)<10)
 	   {
 	    joystick.joy1_y1=0;
@@ -30,13 +31,13 @@ task main()
 	   {
 	    joystick.joy1_y2=0;
 	   }
-     motor[motorLeft] = 100.0/128 * joystick.joy1_y1;
-     motor[motorRight] = 100.0/128 * joystick.joy1_y2;
+     motor[motorRight] = 100.0/128 * joystick.joy1_y1;//right = joy1_y1, the right button
+     motor[motorLeft] = 100.0/128 * joystick.joy1_y2;
 
 		if(joy1Btn(1)) { //runs thrower
 			motor[thrower] = -100.0;
 	   }
-	   
+
 	   if(joy1Btn(3)) { //stops thrower
 			motor[thrower] = 0;
 	   }
@@ -50,7 +51,7 @@ task main()
 	     	servo[grabber1] = (pos1);
 	     	servo[grabber2] = (pos2);
 	   }
-	   
+
 	   if(joy1Btn(4)) { //moves grabbers up
 	     if(pos1 >= 0 && pos1 <= 255){
 	      pos1-=5;
