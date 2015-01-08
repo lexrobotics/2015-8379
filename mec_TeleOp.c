b12bb883bd34fabdde9bb46cbe5ac19d815b1c3b
lexrobotics1 <lexrobotics@gmail.com>
2015-01-08 15:02:15 -0500

    mecanum wheel teleOp and auto motor/servo configuration/setup, ultrasonic paralleling testing.

--- mec_TeleOp.c
+++ mec_TeleOp.c
@@ -1,15 +1,17 @@
-#pragma config(Hubs,  S1, HTMotor,  HTMotor,  HTServo,  none)
+#pragma config(Hubs,  S1, HTMotor,  HTMotor,  HTServo,  HTMotor)
 #pragma config(Sensor, S1,     ,               sensorI2CMuxController)
 #pragma config(Sensor, S2,     touch,          sensorTouch)
 #pragma config(Sensor, S3,     light,          sensorLightActive)
 #pragma config(Sensor, S4,     sonar,          sensorSONAR)
-#pragma config(Motor,  mtr_S1_C1_1,     FrontLeft,     tmotorTetrix, openLoop, encoder)
-#pragma config(Motor,  mtr_S1_C1_2,     BackLeft,      tmotorTetrix, openLoop, encoder)
-#pragma config(Motor,  mtr_S1_C2_1,     BackRight,     tmotorTetrix, openLoop, reversed)
-#pragma config(Motor,  mtr_S1_C2_2,     FrontRight,    tmotorTetrix, openLoop, reversed)
-#pragma config(Servo,  srvo_S1_C3_1,    servo1,               tServoNone)
-#pragma config(Servo,  srvo_S1_C3_2,    servo2,               tServoNone)
-#pragma config(Servo,  srvo_S1_C3_3,    servo3,               tServoNone)
+#pragma config(Motor,  mtr_S1_C1_1,     BackLeft,      tmotorTetrix, openLoop, encoder)
+#pragma config(Motor,  mtr_S1_C1_2,     FrontLeft,     tmotorTetrix, openLoop, encoder)
+#pragma config(Motor,  mtr_S1_C2_1,     Flipper,       tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S1_C2_2,     motorG,        tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S1_C4_1,     FrontRight,    tmotorTetrix, openLoop, reversed, encoder)
+#pragma config(Motor,  mtr_S1_C4_2,     BackRight,     tmotorTetrix, openLoop, reversed, encoder)
+#pragma config(Servo,  srvo_S1_C3_1,    grabber1,             tServoStandard)
+#pragma config(Servo,  srvo_S1_C3_2,    grabber2,             tServoStandard)
+#pragma config(Servo,  srvo_S1_C3_3,    hood,                 tServoStandard)
 #pragma config(Servo,  srvo_S1_C3_4,    servo4,               tServoNone)
 #pragma config(Servo,  srvo_S1_C3_5,    servo5,               tServoNone)
 #pragma config(Servo,  srvo_S1_C3_6,    servo6,               tServoNone)
@@ -20,6 +22,15 @@
 #include "JoystickDriver.c"
 task main()
 {
+	int pos1 = 255;
+	int pos2 = 0;
+//	int pos3 = 150;
+	servo[grabber1]=pos1;
+	servo[grabber2]=pos2;
+//	servo[arm] = pos3;
+
+	int pos4 = 10;
+	servo[hood] = pos4;
 
 	waitForStart();
     while(true){
@@ -47,14 +58,91 @@ FrontRight = Ch3 - Ch1 - Ch4
 RearRight = Ch3 - Ch1 + Ch4
 
 Where:*/
-int Rot = joystick.joy1_x2;//Right joystick X-axis
-int Fwd = joystick.joy1_y1;//-Left joystick Y-axis
-int Side= joystick.joy1_x1;// Left joystick X-axis
+			int Rot = joystick.joy1_x2;//Right joystick X-axis
+			int Fwd = joystick.joy1_y1;//-Left joystick Y-axis
+			int Side= joystick.joy1_x1;// Left joystick X-axis
 
 	    motor[FrontLeft] = 100.0/384 * (-Fwd + Rot - Side);
 			motor[FrontRight] = 100.0/384 * (-Fwd - Rot + Side);
 			motor[BackLeft] = 100.0/384* (-Fwd + Rot + Side);
 			motor[BackRight] = 100.0/384 * (-Fwd - Rot - Side);
+
+			if(joy2Btn(1)) { //runs thrower
+			motor[thrower] = -100.0;
+	   }
+
+	   if(joy2Btn(3)) { //stops thrower
+			motor[thrower] = 0;
+	   }
+
+		if(joy2Btn(4)) { //moves grabbers up
+
+			 	pos1+=5;
+			 	pos2-=5;
+			 	if (pos1 >= 255) pos1=255;
+			 	if(pos2<=0) pos2=0;
+	     	servo[grabber1] = (pos1);
+	     	servo[grabber2] = (pos2);
+	   }
+
+	   if(joy2Btn(2)) { //moves grabbers down
+
+	      pos1-=5;
+	     	pos2+=5;
+	     	if (pos1 <= 0) pos1=0;
+			 	if(pos2>=255) pos2=255;
+	     	servo[grabber1] = (pos1);
+	     	servo[grabber2] = (pos2);
+
+	    }
+
+/*	    if (joy2Btn(5)){// up left move arm up; up left side button
+				if(pos3 >= 0 && pos3 <= 150){
+	      pos3+=5;
+	     	if (pos3 <= 0) pos3=0;
+	     	if(pos3>=150) pos3=150;
+	     	servo[arm] = (pos3);
+	     	wait1Msec(5);
+	    	}
+	    }
+
+	    if (joy2Btn(7)){ //down left move arm down; bottom left side button
+				if(pos3 >= 0 && pos3 <= 150){
+	      pos3-=5;
+	     	if (pos3 <= 0) pos3=0;
+	     	if (pos3 >=150) pos3=150;
+	     	servo[arm] = (pos3);
+	     	wait1Msec(5);
+	    }
+	   }*/
+	   if (joy2Btn(5)){
+/*	     if (pos4 >= 0 && pos4 <= 255)
+	     {
+	       pos4 +=5;//20
+	       if (pos4 <= 0) pos4=0;
+	     	 if (pos4 >=255) pos4=255;
+	     	 servo[hood] = pos4;
+	     	 wait1Msec(500);
+	     }*/
+	     pos4 = 110;
+	     servo[hood] = pos4;
+	   }
+
+	   if (joy2Btn(7)){
+     /*if (pos4 >= 0 && pos4 <= 255)
+	     {
+	       pos4 -=5;
+	       if (pos4 <= 0) pos4=0;
+	     	 if (pos4 >=255) pos4=255;
+	     	 servo[hood] = pos4;
+	     	 wait1Msec(500);
+	     }*/
+	     pos4 = 10;
+	     servo[hood] = pos4;
+	   }
+
+	   if (joy2Btn(6))//runs thrower backward just in case a ball is stuck or something; up right side button
+	     motor[thrower] = 50;
 
      wait1Msec(10);
   }
