1d0be51ed65e909f283b38ae06d9d57b25aebd71
lexrobotics1 <lexrobotics@gmail.com>
2015-02-06 22:35:25 -0500

    finalized autonomous programs, tested and improved TeleOp code, fixed relative values for lift motor and spool servo

--- mec_TeleOp_TankControls.c
+++ mec_TeleOp_TankControls.c
@@ -38,10 +38,11 @@ task main()
 	static float encoderScale=1120.0;
 	int pos1 = 255;
 	int pos2 = 0;
+	int countArm=0;
 	servo[grabber1]=pos1;
 	servo[grabber2]=pos2;
 
-	int pos4 = 40;
+	int pos4 = 50;
 	servo[hood] = pos4;
 	//int armcounter = 0;
 
@@ -100,44 +101,36 @@ Where:*/
 			motor[BackLeft] = 100.0/384* (-Fwd - Rot + Side);
 			motor[BackRight] = 100.0/384 * (-Fwd + Rot - Side);*/
 
-			motor[FrontLeft] = frontback * (-100.0/256 * (-joystick.joy1_y1 - (joystick.joy1_x1 + joystick.joy1_x2) / 2));
-			motor[BackLeft] = frontback * (-100.0/256 * (-joystick.joy1_y1 + (joystick.joy1_x1 + joystick.joy1_x2) / 2));
-			motor[FrontRight] = frontback * (-100.0/256 * (-joystick.joy1_y2 + (joystick.joy1_x1 + joystick.joy1_x2) / 2));
-			motor[BackRight] = frontback * (-100.0/256 * (-joystick.joy1_y2 - (joystick.joy1_x1 + joystick.joy1_x2) / 2));
+
+			if(frontback==1){
+			motor[FrontLeft] = (-100.0/256 * (-joystick.joy1_y1 - (joystick.joy1_x1 + joystick.joy1_x2) / 2));
+			motor[BackLeft] = (-100.0/256 * (-joystick.joy1_y1 + (joystick.joy1_x1 + joystick.joy1_x2) / 2));
+			motor[FrontRight] =  (-100.0/256 * (-joystick.joy1_y2 + (joystick.joy1_x1 + joystick.joy1_x2) / 2));
+			motor[BackRight] = (-100.0/256 * (-joystick.joy1_y2 - (joystick.joy1_x1 + joystick.joy1_x2) / 2));
+			}
+
+			else{
+			motor[FrontRight] = frontback * (-100.0/256 * (-joystick.joy1_y1 - frontback * (joystick.joy1_x1 + joystick.joy1_x2) / 2));
+			motor[BackRight] = frontback * (-100.0/256 * (-joystick.joy1_y1 + frontback * (joystick.joy1_x1 + joystick.joy1_x2) / 2));
+			motor[FrontLeft] = frontback * (-100.0/256 * (-joystick.joy1_y2 + frontback * (joystick.joy1_x1 + joystick.joy1_x2) / 2));
+			motor[BackLeft] = frontback * (-100.0/256 * (-joystick.joy1_y2 - frontback * (joystick.joy1_x1 + joystick.joy1_x2) / 2));
+			}
+
 
 			wait1Msec(10);
 
 
 
 //-Thrower------------------------------------------------------------------------------
-
-		/* if(joy2Btn(1)) { //runs thrower normal way and stop, left of the 4 buttons
-				if ((throwerCount%2) == 0)
-					motor[thrower] = -100.0;
-				else
-					motor[thrower] = 0;
-			throwerCount++;
-			wait1Msec(300);
-	   }
-
-	   if(joy2Btn(3)) { //runs thrower in reverse and stop, right of the 4 buttons
-				if ((throwerCount%2) == 0)
-					motor[thrower] = 50.0;
-				else
-					motor[thrower] = 0;
-			throwerCount++;
-			wait1Msec(300);
-	   }*/
-
-	   if(joy2Btn(1)) {
+	   if(joy1Btn(1)) {
 	     motor[thrower] = -100;
 	}
 
-	   if(joy2Btn(3)) { //runs motor backwards
+	   if(joy1Btn(3)) { //runs motor backwards
 	     motor[thrower] = 0;
 	   }
 
-	   if(joy2Btn(6)) {
+	   if(joy1Btn(5)) {
 	     motor[thrower] = 100;
 	   }
 //-Grabber--------------------------------------------------------------------------------
@@ -159,42 +152,29 @@ Where:*/
 	     	servo[grabber2] = (pos2);
 	   	}
 //-Hood---------------------------------------------------------------------------------------
-	   /*if (joy2Btn(5)){//left side top button, move hood up and down
-				if ((hoodCount%2) == 0)
-					servo[hood]=110;
-				else
-					servo[hood]=10;
-			hoodCount++;
-	     wait1Msec(300);
-	   }*/
 	   if (joy2Btn(5)){
-	     pos4 = 107;
+	     if(countArm<4){
+	       pos4=pos4;
+	     }
+	     else{
+	       pos4 = 107;
+	     }
+	     wait1Msec(10);
 	     servo[hood] = pos4;
 	   }
+
 	   if (joy2Btn(7)){
-	     pos4 = 40;
+	     if(countArm<4){
+	       pos4=pos4;
+	     }
+	     else{
+	       pos4 = 50;
+	     }
+	     wait1Msec(10);
 	     servo[hood] = pos4;
 	   }
 //-Kickstand arm---------------------------------------------------------------------------------------
-	   /*if (joy2Btn(6))//right side top button
-	   {
-	     nMotorEncoder[arm] = 0;
-	     if (kArmCount%2==0)
-	     {
-	       while(nMotorEncoder[arm] < 150){
-	     	 motor[arm] = -50;}
-	     }
-	     else
-	     {
-	       while(abs(nMotorEncoder[arm]) < 130){
-	    	 motor[arm] = 50;}
-	     }
-	     kArmCount++;
-	     motor[arm] = 0;
-	     wait1Msec(300);
-
-	   }*/
-	   if (joy1Btn(6)){
+	   if (joy1Btn(6)){//kickstand arm out
 	   		while(joy1Btn(6))
 	   		{
 	   			motor[arm] = 50;
@@ -213,10 +193,11 @@ Where:*/
 	   	}
 
 //---Lift--------------------------------------------------------------------------------------------------------
-	   if(joy1Btn(1)){ //lift down
+	   if(joy2Btn(1)){ //lift down
+	     countArm-=1;
 	     nMotorEncoder[Lift]=0;
-				motor[Lift]=20;
-				servo[liftRelease] = 10;
+				motor[Lift]=25;
+				servo[liftRelease] = 85;
 	   		while(nMotorEncoder[Lift]<encoderScale)
 	   		{
 	   		}
@@ -224,10 +205,11 @@ Where:*/
 	   		servo[liftRelease] = 127;
 	   	}
 
-	   if(joy1Btn(3)){ //lift up
+	   if(joy2Btn(4)){ //lift up
+	     countArm+=1;
 	     	nMotorEncoder[Lift]=0;
 				motor[Lift]=-30;
-				servo[liftRelease] = 165;
+				servo[liftRelease] = 145;
 	   		while(abs(nMotorEncoder[Lift])<encoderScale)
 	   		{
 	   			//wait1Msec(10);
@@ -236,12 +218,28 @@ Where:*/
 	   		servo[liftRelease] = 127;
 	   	}
 
+	   	if(joy2Btn(6)){
+	   		servo[liftRelease] = 150;
+	   		while(joy2Btn(6))
+	   		{
+	   		}
+	   		servo[liftRelease] = 127;
+	   	}
+
+	   	if(joy2Btn(8)){ //tighten up the servo
+	   		servo[liftRelease] = 90;
+	   		while(joy2Btn(8))
+	   		{
+	   		}
+	   		servo[liftRelease] = 127;
+	   	}
+
 //---Switch Front and Back---------------------------------------------------------------------------------------
-		if (joy1Btn(5)){
+		if (joy1Btn(2)){
 	   	frontback = -1;
 	   	}
-		if (joy1Btn(7)){
-	   	frontback = 1
+		if (joy1Btn(4)){
+	   	frontback = 1;
 	   	}
 
      //wait1Msec(100);
